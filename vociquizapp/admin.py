241bd67440c8f2f20760c1f8c1c008250f505d1e
Patrik <p.friedlos@bluewin.ch>
2023-10-03 22:40:55 +0200

    Setup admin for models

--- vociquizapp/admin.py
+++ vociquizapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Vocabulary, Word, Attempt
 
 # Register your models here.
+admin.site.register(Vocabulary)
+admin.site.register(Word)
+admin.site.register(Attempt)
