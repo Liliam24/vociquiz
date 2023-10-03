24c1442e669dd8be9710f06a3d0be9c78e5a7a9a
Patrik <p.friedlos@bluewin.ch>
2023-10-03 22:37:47 +0200

    Create initial models

--- vociquizapp/models.py
+++ vociquizapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Vocabulary(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+
+class Word(models.Model):
+    vocabulary = models.ForeignKey(Vocabulary, on_delete=models.CASCADE)
+    word = models.CharField(max_length=200)
+    translation = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+class Attempt(models.Model):
+    vocabulary = models.ForeignKey(Vocabulary, on_delete=models.CASCADE)
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    success = models.BooleanField()
