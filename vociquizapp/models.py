092aa7c550472b62e51b01fe6a63301080317f50
Liliam <liliam34.koch@gmail.com>
2023-10-06 10:59:23 +0200

    name change

--- vociquizapp/models.py
+++ vociquizapp/models.py
@@ -5,14 +5,22 @@ class Vocabulary(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
 
+    def __str__(self): 
+        return self.name
+
 class Word(models.Model):
     vocabulary = models.ForeignKey(Vocabulary, on_delete=models.CASCADE)
     word = models.CharField(max_length=200)
     translation = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __str__(self): 
+        return self.word
+
 class Attempt(models.Model):
     vocabulary = models.ForeignKey(Vocabulary, on_delete=models.CASCADE)
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True, blank=True)
     success = models.BooleanField()
+
+     
