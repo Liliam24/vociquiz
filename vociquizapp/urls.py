767e41006bb210b21f5dec4c82072504157a41c7
Liliam <liliam34.koch@gmail.com>
2023-10-06 09:21:57 +0200

    add menu

--- vociquizapp/urls.py
+++ vociquizapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("vocabulary/<int:vocabulary_id>/menu/", views.menu, name="menu"),
 ]
--- vociquizapp/views.py
+++ vociquizapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Vocabulary
 
@@ -9,3 +10,7 @@ def index(request):
         'vocabularies': vocabularies
     }
     return render(request, 'vociquizapp/index.html', context)
+
+def menu(request, vocabulary_id):
+
+    return HttpResponse("Das ist das Menu!")
